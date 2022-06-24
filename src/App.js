e46d79831a36388fe12482d3dd35d06dcaa8144d
taehyeong park <b_tae96@naver.com>
2022-06-24 20:53:10 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import GlobalStyle from "./elem/GlobalStyle";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
