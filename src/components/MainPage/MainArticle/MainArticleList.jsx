1b771831b669d90dcd6c76e6107becbb43ccf6bc
taehyeong park <b_tae96@naver.com>
2022-06-25 21:04:11 +0900

    update_JS . 메인에서 전체 게시글 가져오기 query 작성
    메인 전체에서 게시글 가져오기 query 비동기 통신 작성

--- src/components/MainPage/MainArticle/MainArticleList.jsx
+++ src/components/MainPage/MainArticle/MainArticleList.jsx
@@ -1,10 +1,13 @@
 //패키지 > 컴포넌트 > 커스텀 훅, CSS 컴포넌트 > 모듈(action creator) > CSS
 import React from "react";
+import { useMainArticles } from "../../../custom/reactQuery/useQuery";
 import MainArticleCard from "./MainArticleCard";
 
 const MainArticleList = () => {
   // 데이터 받기 전 예시 arr
   const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // 데이터 가져오기 query
+  // const {data , isLoading, isError, error} = useMainArticles()
 
   return (
     <div>
--- src/custom/reactQuery/useQuery.jsx
+++ src/custom/reactQuery/useQuery.jsx
@@ -1,11 +1,21 @@
 import { api } from "../../shared/api";
 import { useQuery, useMutation, useQueryClient } from "react-query";
-// const queryClient = useQueryClient();
 
 // // Queries
 // const query = useQuery("todos", getTodos);
 
+// 메인 페이지 전체 게시글 가져오기
+export const useMainArticles = () => {
+  const fetcher = async () => {
+    const { data } = await api.get("/main/articles");
+    return data;
+  };
+
+  return useQuery("mainArticles", fetcher);
+};
+
 // // Mutations
+// const queryClient = useQueryClient();
 // const mutation = useMutation(postTodo, {
 //   onSuccess: () => {
 //     // Invalidate and refetch
