696c9465f5e28ec85c61ae29757747559623bdac
taehyeong park <b_tae96@naver.com>
2022-06-25 19:38:05 +0900

    setting . react-query 사용 전 기본 세팅
    query 훅 파일 생성, index.js에서 기본 세팅 작성

---
+++ src/custom/reactQuery/useQuery.jsx
@@ -0,0 +1,14 @@
+import { api } from "../../shared/api";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+// const queryClient = useQueryClient();
+
+// // Queries
+// const query = useQuery("todos", getTodos);
+
+// // Mutations
+// const mutation = useMutation(postTodo, {
+//   onSuccess: () => {
+//     // Invalidate and refetch
+//     queryClient.invalidateQueries("todos");
+//   },
+// });
--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/configStore";
-
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
