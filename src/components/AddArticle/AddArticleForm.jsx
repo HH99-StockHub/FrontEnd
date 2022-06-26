d30de31922b0b9ca7a66610cc421a019fc719bbf
taehyeong park <b_tae96@naver.com>
2022-06-26 22:42:10 +0900

    update_JS . 게시글 작성하기 투자포인트 추가 , 삭제하기 작성
    게시글 작성할 때 투자포인트 칸을 추가하고 삭제하는 코드 작성

--- src/components/AddArticle/AddArticleForm.jsx
+++ src/components/AddArticle/AddArticleForm.jsx
@@ -1,28 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const AddArticleForm = () => {
+  // 투자 포인트 map용 배열
+  const [countArr, setCountArr] = useState([{ key: 0 }]);
+  // key state
+  const [key, setKey] = useState(1);
+  // 게시글 작성하기
+  const writeArticle = (e) => {
+    e.preventDefault();
+  };
+  // 투자 포인트 input 추가하기
+  const addTextarea = () => {
+    setCountArr([...countArr, { key: key }]);
+    setKey(key + 1);
+  };
+  // 투자 포인트 삭제하기
+  const deleteTextarea = (e) => {
+    if (countArr.length !== 1) {
+      const newArr = countArr.filter(
+        (v) => String(v.key) !== String(e.target.id)
+      );
+      setCountArr(newArr);
+    } else {
+      alert("최소 하나의 투자 포인트가 있어야합니다.");
+    }
+  };
   return (
     <WrapForm>
-      <form action="#">
+      <form onSubmit={writeArticle}>
         <div>
           <h3>종목 선택</h3>
           <WrapSelect>
             <input type="text" />
-            <span>aa</span>
+            <span>얼마 상승 등 주가</span>
           </WrapSelect>
         </div>
         <div>
           <h3>제목</h3>
           <input type="text" />
         </div>
-        <div>
-          <h3>투자 포인트</h3>
-          <input type="text" />
-          <br></br>
-          <TextareaText name="text" id="" cols="30" rows="5"></TextareaText>
-        </div>
-        <button type="button">+</button>
+        <h3>투자 포인트</h3>
+        {countArr.map((v, l) => {
+          return (
+            <div key={v.key}>
+              <input type="text" />
+              <span id={v.key} onClick={deleteTextarea}>
+                삭제
+              </span>
+              <br></br>
+              <TextareaText name={v.key} cols="30" rows="5"></TextareaText>
+            </div>
+          );
+        })}
+
+        <button type="button" onClick={addTextarea}>
+          +
+        </button>
         <div>
           <button type="submit">등록하기</button>
           <button type="button">취소</button>
