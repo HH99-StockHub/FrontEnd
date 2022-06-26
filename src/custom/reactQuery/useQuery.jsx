36107f7f00a1df2c08e81bf8fb71af72dcbbf8bc
taehyeong park <b_tae96@naver.com>
2022-06-26 22:49:52 +0900

    update_JS . 메인 / 전체 게시글 부분 GET 요청 query 작성하기
    메인과 전체 게시글을 볼 수 있는 페이지에서 get 요청 할 query 미리 작성하기

--- src/custom/reactQuery/useQuery.jsx
+++ src/custom/reactQuery/useQuery.jsx
@@ -23,6 +23,52 @@ export const useGetPopularArticles = () => {
   return useQuery("popularArticles", fetcher);
 };
 
+// 메인 페이지 수익왕 게시글 가져오기
+export const useGetRichArticles = () => {
+  const fetcher = async () => {
+    const { data } = await api.get("/main/rich/articles");
+    return data;
+  };
+  return useQuery("richArticles", fetcher);
+};
+
+// ALL 전체 게시글 가져오기
+export const useGetAllMainArticles = () => {
+  const fetcher = async () => {
+    const { data } = await api.get("/all/articles");
+    return data;
+  };
+
+  return useQuery("allMainArticles", fetcher);
+};
+
+// ALL 인기 게시글 가져오기
+export const useGetAllPopularArticles = () => {
+  const fetcher = async () => {
+    const { data } = await api.get("/popular/articles");
+    return data;
+  };
+  return useQuery("allPopularArticles", fetcher);
+};
+
+// ALL 수익왕 게시글 가져오기
+export const useGetAllRichArticles = () => {
+  const fetcher = async () => {
+    const { data } = await api.get("/rich/articles");
+    return data;
+  };
+  return useQuery("allRichArticles", fetcher);
+};
+
+// ALL 내 게시글 가져오기
+
+export const useGetAllUserArticles = ({ userId }) => {
+  const fetcher = async () => {
+    const { data } = await api.get(`/user/${userId}/articles`);
+    return data;
+  };
+  return useQuery("allUserArticles", fetcher);
+};
 // // Mutations
 // const queryClient = useQueryClient();
 // const mutation = useMutation(postTodo, {
