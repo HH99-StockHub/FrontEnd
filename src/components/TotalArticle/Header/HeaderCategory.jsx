656da197fafa0d00813ec96d6e0f8f3b5e5a4cff
taehyeong park <b_tae96@naver.com>
2022-06-26 20:40:03 +0900

    update_JS .  카테고리별 링크 연결 및 헤더 메인 이름 변경
    카테고리별 링크 연결 및 URL에 따라 메인 이름 변경되도록 변경

--- src/components/TotalArticle/Header/HeaderCategory.jsx
+++ src/components/TotalArticle/Header/HeaderCategory.jsx
@@ -1,32 +1,68 @@
 //패키지 > 컴포넌트 > 커스텀 훅, CSS 컴포넌트 > 모듈(action creator) > CSS
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
 
 //이미지
 import { ReactComponent as ArticleSvg } from "../../../image/Article.svg";
+
 const HeaderCategory = () => {
+  const { category } = useParams();
+  const [categoryName, setCategoryName] = useState("");
+  useEffect(() => {
+    switch (category) {
+      case "all":
+        setCategoryName("전체 게시판");
+        break;
+      case "popular":
+        setCategoryName("인기 게시판");
+        break;
+      case "rich":
+        setCategoryName("수익왕 게시판");
+        break;
+      case "user":
+        setCategoryName("내 게시글");
+        break;
+      default:
+        break;
+    }
+    return () => {
+      setCategoryName("");
+    };
+  }, [category]);
+
   return (
     <WrapCategory>
-      <CurrentCategory>
-        <ArticleSvg fill="white" />
-        <div>전체 게시판</div>
-      </CurrentCategory>
-      <Category>
-        <ArticleSvg fill="black" />
-        <div>전체 게시판</div>
-      </Category>
-      <Category>
-        <ArticleSvg fill="black" />
-        <div>인기 게시판</div>
-      </Category>
-      <Category>
-        <ArticleSvg fill="black" />
-        <div>수익왕 게시판</div>
-      </Category>
-      <Category>
-        <ArticleSvg fill="black" />
-        <div>내 게시글</div>
-      </Category>
+      <Link to="/">
+        <CurrentCategory>
+          <ArticleSvg fill="white" />
+          <div>{categoryName}</div>
+        </CurrentCategory>
+      </Link>
+      <Link to="/total/all/articles">
+        <Category>
+          <ArticleSvg fill="black" />
+          <div>전체 게시판</div>
+        </Category>
+      </Link>
+      <Link to="/total/popular/articles">
+        <Category>
+          <ArticleSvg fill="black" />
+          <div>인기 게시판</div>
+        </Category>
+      </Link>
+      <Link to="/total/rich/articles">
+        <Category>
+          <ArticleSvg fill="black" />
+          <div>수익왕 게시판</div>
+        </Category>
+      </Link>
+      <Link to="/total/user/articles">
+        <Category>
+          <ArticleSvg fill="black" />
+          <div>내 게시글</div>
+        </Category>
+      </Link>
     </WrapCategory>
   );
 };
