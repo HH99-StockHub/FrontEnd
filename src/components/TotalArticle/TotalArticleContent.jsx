12c9f33308896a1a8b53996bfab5bf8e800931cd
taehyeong park <b_tae96@naver.com>
2022-06-29 16:58:42 +0900

    update_HTML,CSS . 전체 게시판 table 디자인 토대로 작성

--- src/components/TotalArticle/TotalArticleContent.jsx
+++ src/components/TotalArticle/TotalArticleContent.jsx
@@ -6,15 +6,15 @@ const TotalArticleContent = () => {
   return (
     <div>
       <Table>
-        <tr>
-          <th>회사</th>
-          <th>제목</th>
-          <th>날짜</th>
-          <th>조회</th>
-          <th>추천</th>
-          <th>비추천</th>
-          <th>수익률</th>
-        </tr>
+        <WrapTh>
+          <div style={{ width: "72px" }}>회사</div>
+          <div style={{ width: "660px" }}>제목</div>
+          <div style={{ width: "72px" }}>날짜</div>
+          <div style={{ width: "72px" }}>조회</div>
+          <div style={{ width: "92px" }}>추천</div>
+          <div style={{ width: "92px" }}>비추천</div>
+          <div style={{ width: "92px" }}>수익률</div>
+        </WrapTh>
         <TotalArticleList />
       </Table>
     </div>
@@ -23,8 +23,18 @@ const TotalArticleContent = () => {
 
 export default TotalArticleContent;
 
-const Table = styled.table`
-  width: 800px;
+const Table = styled.div`
+  width: 1200px;
   margin: 0 auto;
   text-align: left;
+`;
+
+const WrapTh = styled.div`
+  display: flex;
+  gap: 8px;
+  padding: 16.5px 0;
+  font-size: 12px;
+  font-weight: 400;
+  text-align: center;
+  background-color: #ccc;
 `;
