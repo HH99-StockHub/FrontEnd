73b53120a117b0879f67e62cfb5e852390aa1e82
taehyeong park <b_tae96@naver.com>
2022-07-01 00:37:12 +0900

    update_CSS . 디자인에 맞게 전체 게시글 카테고리 헤더 수정

--- src/components/TotalArticle/Header/Search.jsx
+++ src/components/TotalArticle/Header/Search.jsx
@@ -1,12 +1,30 @@
 //패키지 > 컴포넌트 > 커스텀 훅, CSS 컴포넌트 > 모듈(action creator) > CSS
 import React from "react";
+import styled from "styled-components";
+import { ReactComponent as SearchSvg } from "../../../image/Search.svg";
 
 const Search = () => {
   return (
-    <form>
+    <FormStyle>
       <input type="text" placeholder="관심있는 내용을 검색하세요" />
-    </form>
+      <SearchSvg width="17.49" height="17.49" />
+    </FormStyle>
   );
 };
 
 export default Search;
+
+const FormStyle = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  padding: 11px;
+  width: 333px;
+  height: 40px;
+  border-radius: 25px;
+  border: 1px solid #000;
+  input {
+    width: 100%;
+  }
+`;
