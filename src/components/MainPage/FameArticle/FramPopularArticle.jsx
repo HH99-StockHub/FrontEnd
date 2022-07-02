547d798f3f7af6799260c5a6b7d3830b6df3bbd5
taehyeong park <b_tae96@naver.com>
2022-07-02 17:13:54 +0900

    update_HTML,JS . 디테일 페이지 링크 작성 및 잘못된 파일 구조 수정

--- src/components/MainPage/FameArticle/FramPopularArticle.jsx
+++ src/components/MainPage/FameArticle/FramPopularArticle.jsx
@@ -1,9 +1,11 @@
 //패키지 > 컴포넌트 > 커스텀 훅, CSS 컴포넌트 > 모듈(action creator) > CSS
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { useGetFamePopularArticle } from "../../../custom/reactQuery/useQuery";
 //컴포넌트
 import FramPopularCard from "./FramPopularCard";
+// 쿼리 훅
+import { useGetFamePopularArticle } from "../../../custom/reactQuery/useQuery";
 
 const FramPopularArticle = () => {
   //테스트 arr
@@ -15,7 +17,11 @@ const FramPopularArticle = () => {
       <Title>인기 베스트</Title>
       <WrapCard>
         {data.map((v) => {
-          return <FramPopularCard />;
+          return (
+            <Link to={`/detail/article/${data.id}`}>
+              <FramPopularCard />
+            </Link>
+          );
         })}
       </WrapCard>
     </div>
