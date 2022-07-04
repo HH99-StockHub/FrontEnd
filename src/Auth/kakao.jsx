4caafbd9a54f6994031a857158fca43c3a53287e
taehyeong park <b_tae96@naver.com>
2022-07-04 10:26:10 +0900

    update_JS . key 변경 및 환경 변수 관리하기

--- src/Auth/kakao.jsx
+++ src/Auth/kakao.jsx
@@ -1,3 +1,3 @@
-const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
-const REST_API_KEY = "12c4e96969c4b50ad263268577cdcb76";
+const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
+const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
