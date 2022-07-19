6cda6bd98095c12d74efad80f3fd591a89c9b3aa
taehyeong park <b_tae96@naver.com>
2022-07-19 14:36:34 +0900

    fix . 렌더링 최적화를 위한 SearchArticle 컴포넌트 구조 변경

---
+++ src/components/SearchArticle/SearchArticleContent.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { toastify } from "../../custom/toastify";
+import LoadingSpinner from "../../repeat/LoadingSpinner";
+import TotalArticleList from "../TotalArticle/TotalArticleList";
+import TotalPagenation from "../TotalArticle/TotalPagenation";
+import NotSearch from "./NotSearch";
+import { useGetSearchArticle } from "./useSearchArticleQuery";
+
+const SearchArticleContent = ({ category, keyword, page }) => {
+  const { data, isLoading, isError } = useGetSearchArticle.useGetSearchKeyword(
+    category,
+    keyword,
+    page,
+  );
+  useEffect(() => {
+    if (data?.totalPages === 0) toastify.error("관련 게시글이 없습니다.");
+  }, [data?.totalPages]);
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) toastify.error("게시글 불러오기를 실패했습니다.");
+
+  return (
+    <>
+      {data?.totalPages === 0 ? (
+        <NotSearch keyword={keyword} />
+      ) : (
+        <>
+          <TotalArticleList
+            data={data?.content === undefined ? [] : data.content}
+          />
+          <TotalPagenation
+            category={category}
+            keyword={keyword}
+            nowPage={page}
+            totalPages={data.totalPages}
+            type="search"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default SearchArticleContent;
