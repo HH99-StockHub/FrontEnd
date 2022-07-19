7e6cc5f5c36fc27931b3334ad7695ddfe6a2ab07
sinyoung jeoung <tlsdud210@naver.com>
2022-07-19 11:29:06 +0900

    update.js로딩후페이징맨위로올리기

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { Provider } from "react-redux";
 import store from "./redux/configStore";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ScrollToTop from "./components/ScrollRestoration/ScrollRestoration";
+
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
