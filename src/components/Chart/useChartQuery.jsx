4e528a2a0fe2bacbe29d169b882eaaee53efbb6c
taehyeong park <b_tae96@naver.com>
2022-07-24 01:54:55 +0900

    update_JS. 차트 불러오기 query 작성

---
+++ src/components/Chart/useChartQuery.jsx
@@ -0,0 +1,16 @@
+import { api } from "../../shared/api";
+import { useQuery } from "react-query";
+
+export const useChartQuery = {
+  useGetChartData: (stockName) => {
+    const fetcher = async () => {
+      const { data } = await api.get(`chart/${"삼성전자"}`);
+      return data;
+    };
+    return useQuery(["chart", stockName], fetcher, {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 60,
+      cacheTime: 1000 * 60 * 60,
+    });
+  },
+};
