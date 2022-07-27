770df50d0fc8ff94016eb3c1403ddca777e24537
taehyeong park <b_tae96@naver.com>
2022-07-27 11:43:28 +0900

    update_HTML,CSS,JS . 닉네임 변경하기 API 간이 작성

--- src/repeat/useRepeatQuery.jsx
+++ src/repeat/useRepeatQuery.jsx
@@ -2,6 +2,7 @@ import { api } from "../shared/api";
 import { useMutation } from "react-query";
 import { useSetRecoilState } from "recoil";
 import { alarmList } from "../state/server/alarm";
+import { toastify } from "../custom/toastify";
 
 export const useAlarmMutate = {
   useGetAlarmMutate: () => {
@@ -32,6 +33,24 @@ export const useAlarmMutate = {
           });
           return newList;
         });
+      },
+    });
+  },
+};
+
+export const useHeaderApi = {
+  useChangeNickname: (setChangeNick) => {
+    const fetcher = async (nick) => {
+      const response = api.put("/user/nickname", { newNickname: nick });
+      return response;
+    };
+    return useMutation(fetcher, {
+      onSuccess: (data) => {
+        //닉네임 저장
+        setChangeNick(false);
+      },
+      onError: () => {
+        toastify.error("닉네임이 유효하지 않습니다");
       },
     });
   },
